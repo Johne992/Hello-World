ff1b8417734476606ffbf5067b008e081c7134ae
Johne992 <johnelewis92@gmail.com>
2017-03-16 16:03:03 -0500

    Add files via upload

---
+++ Hello_World.h
@@ -0,0 +1,14 @@
+//Hello World
+
+//includes the header file/library stdio.h that allows us to use the printf function 
+#include<stdio.h>
+
+//The function main
+main()
+{
+	//writes the string by format to stdout, displays it on the screen.
+	printf("Hello World");
+}
+
+//exits the program 
+return 0;
