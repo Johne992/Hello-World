bb3ed1e3f3d5db3ec77e4ad9c95bbc5d60583f48
Johne992 <johnelewis92@gmail.com>
2017-05-24 10:17:48 -0500

    Update Install_Software.ps1

--- Install_Software.ps1
+++ Install_Software.ps1
@@ -1,6 +1,9 @@
-#A goal for this is to probably put it in a clickable GUI that executes and installs each of the files. Its possible it could also
-#...check to see if the files are already installed and then run the .exe or .msi
+<#
+.SYNOPSIS
+A goal for this is to probably put it in a clickable GUI that executes and installs each of the files. Its possible it could also
+...check to see if the files are already installed and then run the .exe or .msi
 
+#>
 #This stores the location of the file and installs it silently (/s) , (/v) passes arguments to msiexec
 #...so it passes (/qn) which means run with no UI
 S:\Druva\inSync-5.9-r51251.msi #/S /v/qn
