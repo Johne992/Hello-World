6f83e77897346ffddf0c98684cd58cc7463b3a9c
John Lewis <johnelewis92@gmail.com>
2017-05-25 11:08:16 -0500

    Update Cities_In_LA.py

--- Cities_In_LA.py
+++ Cities_In_LA.py
@@ -1,6 +1,18 @@
 #Concept: Today's program will parse wikipedia and pull the names of all the cities. I may make it more complex and see if I can add 
 #... additional information and sorting by specific values. 
 
+#Outline:
+#Import webscraper
+#import library for opening Urls
+#Make sure code can interface with wikipedia
+#Request the headers of the wiki
+#Open the wiki page
+#Run web scraper across page
+#Locate specifics for table and column titles
+#Pull row data from column titles
+#Output data once pulled.
+#....
+
 
 from bs4 import BeautifulSoup
 import urllib2
